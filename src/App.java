e6e1cb88f16f540075f43c6f74c0f4bb5dbced21
0rthodox <long.popov@gmail.com>
2020-03-19 11:30:00 +0300

    release

--- src/App.java
+++ src/App.java
@@ -2,8 +2,13 @@ import javafx.application.Application;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import tictac.View;
+import utils.FileManager;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 
 public class App extends Application {
@@ -11,6 +16,8 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setResizable(false);
+        primaryStage.setTitle("The Match of the Century");
+        primaryStage.getIcons().add(FileManager.readImage("resources/x.png"));
         BooleanProperty startPressedProperty = new SimpleBooleanProperty(false);
         BooleanProperty restartProperty = new SimpleBooleanProperty(false);
         BooleanProperty exitPressedProperty = new SimpleBooleanProperty(false);
--- src/tictac/View.java
+++ src/tictac/View.java
@@ -2,12 +2,18 @@ package tictac;
 
 import cell.Cell;
 import javafx.beans.property.*;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import stats.Stats;
 import stats.StatsManager;
+import utils.FileManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,11 +26,11 @@ public class View {
     private BooleanProperty restartProperty = new SimpleBooleanProperty(false);
     private ImageManager imageManager = new ImageManager();
     private ViewModel viewModel = new ViewModel();
-    private ObjectProperty<State> hasWinner = new SimpleObjectProperty<>();
     private Alert winnerAlert = new Alert(Alert.AlertType.INFORMATION);
     private final int GAP = 10;
 
     public View() {
+        ObjectProperty<State> hasWinner = new SimpleObjectProperty<>();
         hasWinner.bindBidirectional(viewModel.getHasWinnerProperty());
         hasWinner.addListener((observable, oldValue, newValue) -> {
             StatsManager statsManager = new StatsManager();
@@ -56,7 +62,7 @@ public class View {
         });
     }
 
-    public GridPane getChoicePane(BooleanProperty startPressedProperty, BooleanProperty exitPressedProperty) {
+    public VBox getChoicePane(BooleanProperty startPressedProperty, BooleanProperty exitPressedProperty) {
         GridPane choicePane = new GridPane();
         choicePane.setHgap(GAP);
         choicePane.setVgap(GAP);
@@ -65,6 +71,9 @@ public class View {
         TextField textFieldDCAM = new TextField();
         BooleanProperty startPressed = new SimpleBooleanProperty(false);
         BooleanProperty exitPressed = new SimpleBooleanProperty(false);
+
+        textFieldDASR.setPromptText("Anonymous");
+        textFieldDCAM.setPromptText("Anonymous");
 
         textFieldDASR.textProperty().bindBidirectional(playerDASRProperty);
         textFieldDCAM.textProperty().bindBidirectional(playerDCAMProperty);
@@ -79,15 +88,21 @@ public class View {
         startButton.setOnAction(event -> startPressed.setValue(true));
         exitButton.setOnAction(event -> exitPressed.setValue(true));
 
-        choicePane.add(new Label("DASR's player:"), 0, 0);
+        Label welcoming = new Label("Welcome to");
+        Label title = new Label("The Match of the Century");
+        title.setFont(new Font("Comic Sans MS", 30));
+        title.setTextFill(Color.rgb(115, 102, 189));
+        title.setAlignment(Pos.CENTER);
+
+        choicePane.add(new Label("DASR player:"), 0, 0);
         choicePane.add(textFieldDASR, 1, 0);
-        choicePane.add(new Label("DCAM's player:"), 0, 1);
+        choicePane.add(new Label("DCAM player:"), 0, 1);
         choicePane.add(textFieldDCAM, 1, 1);
         choicePane.add(new Label("Show statistics:"), 0, 3);
         choicePane.add(statistics, 1, 3);
         choicePane.add(startButton, 0, 4);
         choicePane.add(exitButton, 1, 4);
-        return choicePane;
+        return new VBox(welcoming, title, choicePane);
     }
     public GridPane getGamePane(BooleanProperty restartProperty) {
         restartProperty.bindBidirectional(this.restartProperty);
@@ -112,6 +127,8 @@ public class View {
             labels.add(labeledStats);
         }
         Stage statsStage = new Stage();
+        statsStage.getIcons().add(FileManager.readImage("resources/x.png"));
+        statsStage.setTitle("Stats");
         GridPane statsPane = new GridPane();
         statsPane.setHgap(GAP);
         statsPane.setVgap(GAP);
